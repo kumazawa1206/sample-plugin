4b6635c51cef2f7a877668ecd1a25ce1530bdc05
yu ji <leon5617@gmail.com>
2023-07-12 11:46:36 +0900

    SetLevelで任意のLevelに設定できるように変更。

--- src/main/java/plugin/firstplugin/SetLevelCommand.java
+++ src/main/java/plugin/firstplugin/SetLevelCommand.java
@@ -5,12 +5,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class LevelUpCommand implements CommandExecutor {
+public class SetLevelCommand implements CommandExecutor {
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
-      player.setLevel(100);
+      //引数が１個の時、文字列を数字に変換して取得する。
+      //引数が１個じゃなければ"Nooooooo!!"を返す。
+      if (args.length == 1) {
+        player.setLevel(Integer.parseInt(args[0]));
+      } else {
+        player.sendMessage("No!!");
+      }
     }
     return false;
   }
